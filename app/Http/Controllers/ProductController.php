2ed214a87386b95f6dbcbd1fd488088391032a76
ksh <seongs70@naver.com>
2019-05-17 16:48:34 +0900

    Product destroy

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -107,6 +107,7 @@ class ProductController extends Controller
      */
     public function destroy(product $product)
     {
-        //
+        $product->delete();
+        return response(null,Response::HTTP_NO_CONTENT);
     }
 }
